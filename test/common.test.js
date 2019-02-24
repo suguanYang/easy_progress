6b35df4f5d42346fdfc653876e94b1503caf80c9
Donnie Wang <donnie.wang@striking.ly>
2019-02-24 20:44:59 +0800

    add test

--- test/common.test.js
+++ test/common.test.js
@@ -1,5 +1,17 @@
-const progressBar = require("../lib/progress");
-const assert = require("assert");
+const { makeProgress } = require("../lib/progress");
 
-// function lazyExceProc(proc, )
+test("It should throw a Error", () => {
+  const test = () => makeProgress({
+    iteration: -1,
+  });
+  expect(test).toThrowError("iteration must be a positive number");
+});
 
+test("It should return a 50% progress", () => {
+  const fill = String.fromCharCode(9608);
+  const test = makeProgress({
+    fill,
+    caculatedPercent: 0.5
+  });
+  expect(test()).toBe("Progress: |█████████████████████████-------------------------| 50.00% Complete");
+});
